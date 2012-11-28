ed7550297bcc4825446d02aabd47dc0907b30425
Philippe Dionne <dionne.phil@gmail.com>
2012-11-28 16:22:19 -0500

    Create authorization migration

---
+++ db/migrate/20121128195937_create_authorizations.rb
@@ -0,0 +1,9 @@
+class CreateAuthorizations < ActiveRecord::Migration
+  def change
+    create_table :authorizations do |t|
+      t.string :uid
+      t.string :provider
+      t.integer :user_id
+    end
+  end
+end
