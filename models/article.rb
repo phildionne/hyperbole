925cc1c41ec9fe2f8ecfb64eaa84d97822e93643
Philippe Dionne <dionne.phil@gmail.com>
2012-11-28 16:16:47 -0500

    Add scopes and validations

--- models/article.rb
+++ models/article.rb
@@ -1,8 +1,29 @@
 class Article < ActiveRecord::Base
-  attr_accessible :url, :title
+  attr_accessible :uri, :title
+
+  validates :uri, :presence => true, :format => { :with => URI::regexp }
+
   belongs_to :user
 
-  def self.today
-    where('created_at < ?', Time.now)
+  before_create           :cleanup_uri
+
+  default_scope           order('DATE(created_at) DESC')
+
+  scope :previous_year,   lambda { where('DATE(created_at) >= ?', 1.year.ago ) }
+  scope :previous_month,  lambda { where('DATE(created_at) >= ?', 1.month.ago ) }
+  scope :previous_week,   lambda { where('DATE(created_at) >= ?', 1.week.ago ) }
+  # scope :yesterday,       lambda { where('DATE(created_at) = ?', 1.day.ago..Time.now.at_beginning_of_day)} # FIX this
+  scope :today,           lambda { where('DATE(created_at) >= ?', Time.now.at_beginning_of_day) }
+
+  # scope :group_by_day,    group(['DATE(created_at)'])
+
+
+  protected
+
+  def cleanup_uri
+    uri = URI.split(self.uri)
+    uri[5].sub!(%r(^\/{1}), '') if uri[2].nil?
+    uri[0] << '://'
+    self.uri = uri.join
   end
 end
