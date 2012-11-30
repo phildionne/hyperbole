c51be3f87baa3f82366f412d332e56064aa4c05c
Philippe Dionne <dionne.phil@gmail.com>
2012-11-30 13:49:03 -0500

    Complete user model

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,28 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  attr_accessible :name, :time_zone, :utc_offset
+
+  validates_presence_of :name
+
+  has_many :authentications, :dependent => :destroy
+  has_many :articles, :dependent => :destroy
+
+
+  def self.find_or_create_from_auth_hash(auth, user = nil)
+
+    # Create user if current_user is nil
+    user ||= User.create!(  :name => auth[:info][:name],
+                            :location => auth[:info][:location],
+                            :time_zone => auth[:extra][:raw_info][:time_zone],
+                            :utc_offset => auth[:extra][:raw_info][:utc_offset]
+                            )
+
+    # Create authentication
+    authentication = Authentication.new
+    authentication.user = user
+    authentication.uid = auth[:uid]
+    authentication.provider = auth[:provider]
+    authentication.save!
+
+    user
+  end
 end
