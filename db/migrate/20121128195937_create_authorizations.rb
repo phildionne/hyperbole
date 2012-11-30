8fa45f118d5cbe2c6646883886c76171b0ed4d52
Philippe Dionne <dionne.phil@gmail.com>
2012-11-30 14:02:13 -0500

    Delete authorization model

--- db/migrate/20121128195937_create_authorizations.rb
+++
@@ -1,9 +0,0 @@
-class CreateAuthorizations < ActiveRecord::Migration
-  def change
-    create_table :authorizations do |t|
-      t.string :uid
-      t.string :provider
-      t.integer :user_id
-    end
-  end
-end
