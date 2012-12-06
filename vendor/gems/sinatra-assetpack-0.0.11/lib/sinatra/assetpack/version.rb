80686234a5172f16d8b4b50fa591853a502c8da8
Philippe Dionne <dionne.phil@gmail.com>
2012-12-06 10:48:40 -0500

    Move vendor/ to vendor/bundle

--- vendor/gems/sinatra-assetpack-0.0.11/lib/sinatra/assetpack/version.rb
+++
@@ -1,7 +0,0 @@
-module Sinatra
-  module AssetPack
-    def self.version
-      "0.0.11"
-    end
-  end
-end
--- vendor/gems/sinatra-assetpack-0.0.11/test/cache_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../test_helper', __FILE__)
-
-class CacheTest < UnitTest
-  test "Compressed js caching" do
-    app.set :reload_templates, false
-    JSMin.expects(:minify).times(1).returns ""
-    3.times { get '/js/app.js' }
-  end
-end
