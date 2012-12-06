fed1256edd977234753bdd4518889d3db8ea1949
Philippe Dionne <dionne.phil@gmail.com>
2012-12-06 10:51:31 -0500

    Add a authentication, time_zone support and fullname helper method

--- models/user.rb
+++ models/user.rb
@@ -1,28 +1,25 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :time_zone, :utc_offset
-
-  validates_presence_of :name
+  attr_accessible :name, :location, :time_zone
 
   has_many :authentications, :dependent => :destroy
   has_many :articles, :dependent => :destroy
 
+  validates_presence_of :name, :location
 
-  def self.find_or_create_from_auth_hash(auth, user = nil)
-
+  def self.find_or_create_from_auth(auth, user = nil)
     # Create user if current_user is nil
     user ||= User.create!(  :name => auth[:info][:name],
                             :location => auth[:info][:location],
-                            :time_zone => auth[:extra][:raw_info][:time_zone],
-                            :utc_offset => auth[:extra][:raw_info][:utc_offset]
+                            :time_zone => auth[:extra][:raw_info][:time_zone] # Fallback to default Time.zone if empty
                             )
-
-    # Create authentication
-    authentication = Authentication.new
-    authentication.user = user
-    authentication.uid = auth[:uid]
-    authentication.provider = auth[:provider]
-    authentication.save!
+    # Create new authentication
+    Authentication.create_from_user_and_auth!(user, auth)
 
     user
+  end
+
+  def fullname_or_american_first_name
+    name = self.name.split[0] unless self.name.split.count >= 3
+    name ||= fullname
   end
 end
