d971198f4dbb8ace6033105b9203246572ce6b72
Philippe Dionne <dionne.phil@gmail.com>
2012-12-11 17:41:11 -0500

    Moved sprockets configuration into app's configure block

--- config/sprockets.rb
+++
@@ -1,7 +0,0 @@
-Sinatra::Sprockets.configure do |config|
-  config.digest = true              # Append a digest to URLs
-  config.compile = true             # On-the-fly compilation
-  config.manifest_path = 'assets'
-  config.css_compressor = true     # CSS compressor instance
-  config.js_compressor = true      # JS compressor instance
-end
